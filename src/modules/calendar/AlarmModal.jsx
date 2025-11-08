1223943ea0b64d5ab3836d8cea63be4347621815
zend-21 <ksi21jiho@gmail.com>
2025-11-08 10:36:14 +0900

    fix: pass setProfile to LoginModal

--- src/modules/calendar/AlarmModal.jsx
+++ src/modules/calendar/AlarmModal.jsx
@@ -2087,7 +2087,7 @@ const AlarmModal = ({ isOpen, scheduleData, onSave, onClose }) => {
                                 opacity: alarm.enabled !== false ? 1 : 0.5,
                                 wordBreak: 'break-all',
                                 lineHeight: '1.3',
-                                maxWidth: '10em',
+                                maxWidth: '9em',
                                 display: 'inline-block'
                               }}>
                                 {alarm.title || '제목 없음'}
